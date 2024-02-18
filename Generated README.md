87b0926396716ea08a58a124fdb1be3247be5018
Amr Shehata <amr_shehata@hotmail.com>
2024-02-18 16:01:52 +0000

    Added the remaining contents of the table

--- Generated README.md
+++ Generated README.md
@@ -1,2 +1,35 @@
-# testing
+# Testing the rest of table of contents
+
+## Description
+undefined
+
+## Table of Contents
+- [Description](#description)
+- [Installation](#installation)
+- [Usage](#usage)
+- [License](#license)
+- [Contributing](#contributing)
+- [Tests](#tests)
+- [Questions](#questions)
+
+## Installation
+undefined
+
+## Usage
+undefined
+
+## License
+This project is licensed under the undefined license.
+
+## Contributing
+undefined
+
+## Tests
+undefined
+
+## Questions
+GitHub: [undefined](https://github.com/undefined)
+
+For additional questions, please email [undefined](mailto:undefined).
+
 
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -2,6 +2,39 @@
 function generateMarkdown(data) {
   return `# ${data.title}
 
+## Description
+${data.description}
+
+## Table of Contents
+- [Description](#description)
+- [Installation](#installation)
+- [Usage](#usage)
+- [License](#license)
+- [Contributing](#contributing)
+- [Tests](#tests)
+- [Questions](#questions)
+
+## Installation
+${data.installation}
+
+## Usage
+${data.usage}
+
+## License
+This project is licensed under the ${data.license} license.
+
+## Contributing
+${data.contribution}
+
+## Tests
+${data.tests}
+
+## Questions
+GitHub: [${data.github}](https://github.com/${data.github})
+
+For additional questions, please email [${data.email}](mailto:${data.email}).
+
+
 `;
 }
 

bf2a20a53998794fa8d94d117fbc7d3abaa961c7
Amr Shehata <amr_shehata@hotmail.com>
2024-02-18 15:52:21 +0000

    added one question to test generation

--- index.js
+++ index.js
@@ -5,17 +5,32 @@ const generateMarkdown = require("./utils/generateMarkdown");
 
 // array of questions for user
 const questions = [
+    {
+        type: "input",
+        name: "title",
+        message: "Type in the title of your project?"
+    },
 
 ];
 
 // function to write README file
 function writeToFile(fileName, data) {
+    fs.writeFileSync(fileName, data);
 }
 
 // function to initialize program
 function init() {
+    inquirer.prompt(questions)
+        .then((answers) => {
+        const readmeContent = generateMarkdown(answers);
+        writeToFile("Generated README.md", readmeContent);
+        console.log("Generated README.md successfully generated!");
+        })
+        .catch((error) => {
+        console.error("Error occurred:", error);
+        });
+    }
 
-}
 
 // function call to initialize program
 init();
